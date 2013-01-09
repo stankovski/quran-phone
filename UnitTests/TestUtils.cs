8d8350dee912a2e2778f70f2f4f94b0041360241
Denis Stankovski <stankovski@users.noreply.github.com>
2013-01-09 12:06:48 -0800

    Started conversion

---
+++ UnitTests/TestUtils.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestUtils
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            int a = 1;
+            int b = 2;
+
+            int c = a + b;
+
+            Assert.IsTrue(c == 4);
+        }
+    }
+}
---
+++ WindowsPhone/Data/Mappings.cs
@@ -0,0 +1,31 @@
+﻿using QuranPhone.Common;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuranPhone.Data
+{
+    public class Mappings
+    {
+        public TableMapping AyahBoundsMapping()
+        {
+            //string COL_PAGE = "page_number";
+            //string COL_LINE = "line_number";
+            //string COL_SURA = "sura_number";
+            //string COL_AYAH = "ayah_number";
+            //string COL_POSITION = "position";
+            //string MIN_X = "min_x";
+            //string MIN_Y = "min_y";
+            //string MAX_X = "max_x";
+            //string MAX_Y = "max_y";
+            //string GLYPHS_TABLE = "glyphs";
+            //TableMapping mapping = new TableMapping(typeof(AyahBounds));
+            //mapping.TableName = GLYPHS_TABLE;
+            //mapping.
+            return null;
+        }
+    }
+}
