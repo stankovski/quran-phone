78654af9a8565c828460631e79c7a984c7389d33
Denis Stankovski <stankovski@users.noreply.github.com>
2013-01-19 15:12:40 -0800

    Started work on page view

---
+++ WindowsPhone/UI/CachedImage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Windows.Media.Imaging;
+
+namespace QuranPhone.UI
+{
+    public partial class CachedImage : UserControl, INotifyPropertyChanged
+    {
+        public CachedImage()
+        {
+            InitializeComponent();
+        }
+
+        public Uri ImageSource {
+            get { return (Uri)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); } 
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register("ImageSource",
+            typeof(Uri), typeof(CachedImage), new PropertyMetadata(
+            new PropertyChangedCallback(changeSource))); 
+        
+        private static void changeSource(DependencyObject source, DependencyPropertyChangedEventArgs e) 
+        { 
+            (source as CachedImage).UpdateSource(e.NewValue as Uri); 
+        }
+
+        private void UpdateSource(Uri source)
+        {
+            BitmapImage imageSource = new BitmapImage();
+            imageSource.UriSource = source;
+            image.Source = imageSource;
+        }
+
+        #region INotifyPropertyChanged Members
+
+        /// <summary>
+        /// Raised when a property on this object has a new value.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises this object's PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">The property that has a new value.</param>
+        protected virtual void OnPropertyChanged<T>(Expression<Func<T>> expression)
+        {
+            string propertyName = GetPropertyName(expression);
+            OnPropertyChanged(propertyName);
+        }
+
+        /// <summary>
+        /// Raises this object's PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">The property that has a new value.</param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        protected virtual bool IsProperty<T>(Expression<Func<T>> expression, string name)
+        {
+            string propertyName = GetPropertyName(expression);
+            return propertyName == name;
+        }
+
+        /// <summary>
+        /// Get the string name for the property
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        protected string GetPropertyName<T>(Expression<Func<T>> expression)
+        {
+            MemberExpression memberExpression = (MemberExpression)expression.Body;
+            return memberExpression.Member.Name;
+        }
+
+        #endregion // INotifyPropertyChanged Members
+    }
+}
---
+++ WindowsPhone/ViewModels/PageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using QuranPhone.Resources;
+using QuranPhone.Data;
+using System.Windows.Controls;
+
+namespace QuranPhone.ViewModels
+{
+    public class PageViewModel : ViewModelBase
+    {
+        public PageViewModel()
+        {
+        }
+
+        private Uri imageSource;
+        public Uri ImageSource
+        {
+            get { return imageSource; }
+            set
+            {
+                if (value == imageSource)
+                    return;
+
+                imageSource = value;
+
+                base.OnPropertyChanged(() => ImageSource);
+            }
+        }
+    }
+}
