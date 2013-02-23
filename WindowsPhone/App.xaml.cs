8e0f7b7a89cdeb36165fe7157ef5995a77ff4117
Denis Stankovski <Denis Stankovski@localhost>
2013-02-23 12:39:33 -0800

    Added page transitions

--- WindowsPhone/App.xaml.cs
+++ WindowsPhone/App.xaml.cs
@@ -194,7 +194,7 @@ namespace QuranPhone
 
             // Create the frame but don't set it as RootVisual yet; this allows the splash
             // screen to remain active until the application is ready to render.
-            RootFrame = new PhoneApplicationFrame();
+            RootFrame = new TransitionFrame();
             RootFrame.Navigated += CompleteInitializePhoneApplication;
 
             // Handle navigation failures
