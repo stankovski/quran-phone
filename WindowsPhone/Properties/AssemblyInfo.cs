58f80ea5eb33eaee739cc464b30396fab2828651
kemas dimas <kemas.dimas@live.com>
2013-06-08 06:19:54 +0700

    Adding Indonesian resources, changing few project options

--- WindowsPhone/Properties/AssemblyInfo.cs
+++ WindowsPhone/Properties/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Resources;
 [assembly: AssemblyTitle("Quran Phone")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("IslamWP")]
 [assembly: AssemblyProduct("Quran Phone")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("IslamWP ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.1")]
-[assembly: AssemblyFileVersion("0.2.1")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
