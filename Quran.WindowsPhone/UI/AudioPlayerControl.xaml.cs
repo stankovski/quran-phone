1f47d065bbd3cfe88f099d716e1319048b50be25
denis stankovski <denis.stankovski@gmail.com>
2013-11-23 22:06:29 -0800

    Started implementation of audio repeat

--- Quran.WindowsPhone/UI/AudioPlayerControl.xaml.cs
+++ Quran.WindowsPhone/UI/AudioPlayerControl.xaml.cs
@@ -10,6 +10,11 @@ namespace Quran.WindowsPhone.UI
         public AudioPlayerControl()
         {
             InitializeComponent();
+            this.PlayButtonGrid.Visibility = System.Windows.Visibility.Collapsed;
+            this.PauseButtonGrid.Visibility = System.Windows.Visibility.Collapsed;
+            this.RepeatButtonGrid.Visibility = System.Windows.Visibility.Collapsed;
+            this.NoRepeatButtonGrid.Visibility = System.Windows.Visibility.Collapsed;
+            this.StopButtonGrid.Visibility = System.Windows.Visibility.Collapsed;
         }
 
         public AudioState AudioState
@@ -33,14 +38,21 @@ namespace Quran.WindowsPhone.UI
                     case AudioState.Stopped:
                         thisControl.PlayButtonGrid.Visibility = Visibility.Collapsed;
                         thisControl.PauseButtonGrid.Visibility = Visibility.Collapsed;
+                        thisControl.RepeatButtonGrid.Visibility = Visibility.Collapsed;
+                        thisControl.NoRepeatButtonGrid.Visibility = Visibility.Collapsed;
+                        thisControl.StopButtonGrid.Visibility = Visibility.Collapsed;
                         break;
                     case AudioState.Playing:
                         thisControl.PlayButtonGrid.Visibility = Visibility.Collapsed;
                         thisControl.PauseButtonGrid.Visibility = Visibility.Visible;
+                        thisControl.RepeatButtonGrid.Visibility = Visibility.Visible;
+                        thisControl.NoRepeatButtonGrid.Visibility = Visibility.Collapsed;
+                        thisControl.StopButtonGrid.Visibility = Visibility.Visible;
                         break;
                     case AudioState.Paused:
                         thisControl.PlayButtonGrid.Visibility = Visibility.Visible;
                         thisControl.PauseButtonGrid.Visibility = Visibility.Collapsed;
+                        thisControl.StopButtonGrid.Visibility = Visibility.Visible;
                         break;
                 }
             }
@@ -52,6 +64,14 @@ namespace Quran.WindowsPhone.UI
                 QuranApp.NativeProvider.AudioProvider.Pause();
             else if (AudioState == AudioState.Paused)
                 QuranApp.NativeProvider.AudioProvider.Play();
+        }
+
+        private void OnControlManipulationComplete(object sender, ManipulationCompletedEventArgs e)
+        {
+            e.Handled = true;
+            var velocities = e.FinalVelocities;
+            if (velocities.LinearVelocity.X > 200)
+                MessageBox.Show("Swipe right");
         }
     }
 }
