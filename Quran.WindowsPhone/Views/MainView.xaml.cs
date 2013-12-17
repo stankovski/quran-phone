77b927b5626e238f87019444647f20125eeccb9c
denis stankovski <denis.stankovski@gmail.com>
2013-12-16 21:17:13 -0800

    Release 0.4.2

--- Quran.WindowsPhone/Views/MainView.xaml.cs
+++ Quran.WindowsPhone/Views/MainView.xaml.cs
@@ -76,8 +76,8 @@ namespace Quran.WindowsPhone.Views
                     @"Assalamu Aleikum,
 
 Thank you for downloading Quran Phone. Please note that this is a BETA release and is still work in progress. 
-New in Version 0.4.1:
-* Bug fixes related to search and error logging
+New in Version 0.4.2:
+* Bug fixes
 
 If you find any issues with the app or would like to provide suggestions, please use Contact Us option available via the menu. 
 
